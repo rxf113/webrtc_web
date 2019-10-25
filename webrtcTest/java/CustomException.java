972ea2519b875af48d3e492233b1eba0d133e88f
rxf113 <36568064+rxf113@users.noreply.github.com>
2019-10-25 17:25:02 +0800

    Add files via upload

---
+++ webrtcTest/java/CustomException.java
@@ -0,0 +1,7 @@
+package com.netty;
+
+public class CustomException extends RuntimeException{
+    public CustomException(String message) {
+        super(message);
+    }
+}
---
+++ webrtcTest/java/RoomInfo.java
@@ -0,0 +1,43 @@
+package com.netty;
+
+import io.netty.channel.Channel;
+
+public class RoomInfo {
+    private String roomName;
+    private Channel callChannel;
+    private Channel receiveChannel;
+    //1等待连接 2连通 3非正常断开
+    private Integer status;
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public Channel getCallChannel() {
+        return callChannel;
+    }
+
+    public void setCallChannel(Channel callChannel) {
+        this.callChannel = callChannel;
+    }
+
+    public Channel getReceiveChannel() {
+        return receiveChannel;
+    }
+
+    public void setReceiveChannel(Channel receiveChannel) {
+        this.receiveChannel = receiveChannel;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
