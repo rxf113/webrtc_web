57d8265d9ff90dfacd31d637a6b0f1d6b508be2b
我是个文盲 <1131310577@qq.com>
2021-05-15 00:45:21 +0800

    feat add heartbeat

--- src/main/java/com/rxf113/chat/Server/CustomChannelInitializer.java
+++ src/main/java/com/rxf113/chat/Server/CustomChannelInitializer.java
@@ -1,11 +1,22 @@
 package com.rxf113.chat.Server;
 
+import com.alibaba.fastjson.JSONObject;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.rxf113.chat.Server.CustomChannelInboundHandler.heartBeatData;
 
 class CustomChannelInitializer extends ChannelInitializer<SocketChannel> {
     @Override
@@ -16,5 +27,21 @@ class CustomChannelInitializer extends ChannelInitializer<SocketChannel> {
 
         socketChannel.pipeline().addLast(new WebSocketServerProtocolHandler("/ws"));
         socketChannel.pipeline().addLast(new CustomChannelInboundHandler());
+        //心跳检测
+        socketChannel.pipeline().addFirst(new IdleStateHandler(60, 0, 0, TimeUnit.SECONDS));
+
+        socketChannel.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+            @Override
+            public void userEventTriggered(ChannelHandlerContext ctx,
+                                           Object evt) throws Exception {
+                if (evt instanceof IdleStateEvent) {
+                    System.out.println("send heart beat ...");
+                    ctx.channel().writeAndFlush(new TextWebSocketFrame(JSONObject.toJSONString(heartBeatData)))
+                            .addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
+                } else {
+                    super.userEventTriggered(ctx, evt);
+                }
+            }
+        });
     }
 }
